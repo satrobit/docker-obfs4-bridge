3e8088ce5b2d49eee648d941c0b1024c19191733
rotor <1106-rotor@gitlab.torproject.org>
2021-02-20 22:41:08 +0100

    [#1] Make image more configurable
    In addition to basic variables OR_PORT, PT_PORT and EMAIL, it is now
    possible to enable additional variables support for extra configuration
    tweaks. Having OBFS4_ENABLE_ADDITIONAL_VARIABLES set to 1, all the
    variables prefixed with OBFS4V_ (with their values) are passed to torrc
    (with prefix removed). That make it possible to configure virtually
    everything using environment variables.
    
    For example:
    OBFS4_ENABLE_ADDITIONAL_VARIABLES=1
    OBFS4V_AccountingStart=day 0:00
    OBFS4V_AccountingMax=10 GBytes
    OBFS4V_RelayBandwidthRate=200 KBytes
    OBFS4V_RelayBandwidthBurst=1000 KBytes
    
    will generate:
    AccountingStart day 0:00
    AccountingMax 10 GBytes
    RelayBandwidthRate 200 KBytes
    RelayBandwidthBurst 1000 KBytes
    
    in /etc/tor/torrc. The variables or put after the basic variables and or
    displayed (with the corresponding values) during the container start.

--- start-tor.sh
+++ start-tor.sh
@@ -2,6 +2,24 @@
 
 echo "Using OR_PORT=${OR_PORT}, PT_PORT=${PT_PORT}, and EMAIL=${EMAIL}."
 
+ADDITIONAL_VARIABLES_PREFIX="OBFS4V_"
+ADDITIONAL_VARIABLES=
+
+if [[ "$OBFS4_ENABLE_ADDITIONAL_VARIABLES" == "1" ]]
+then
+    ADDITIONAL_VARIABLES="# Additional properties from processed '$ADDITIONAL_VARIABLES_PREFIX' environment variables"
+    echo "Additional properties from '$ADDITIONAL_VARIABLES_PREFIX' environment variables processing enabled"
+
+    IFS=$'\n'
+    for V in $(env | grep "^$ADDITIONAL_VARIABLES_PREFIX"); do
+        VKEY_ORG="$(echo $V | cut -d '=' -f1)"
+        VKEY="${VKEY_ORG#$ADDITIONAL_VARIABLES_PREFIX}"
+        VVALUE="$(echo $V | cut -d '=' -f2)"
+        echo "Overriding '$VKEY' with value '$VVALUE'"
+        ADDITIONAL_VARIABLES="$ADDITIONAL_VARIABLES"$'\n'"$VKEY $VVALUE"
+    done
+fi
+
 cat > /etc/tor/torrc << EOF
 RunAsDaemon 0
 # We don't need an open SOCKS port.
@@ -24,6 +42,8 @@ ServerTransportListenAddr obfs4 0.0.0.0:${PT_PORT}
 
 # The variable "EMAIL" is replaced with the operator's email address.
 ContactInfo ${EMAIL}
+
+$ADDITIONAL_VARIABLES
 EOF
 
 echo "Starting tor."
